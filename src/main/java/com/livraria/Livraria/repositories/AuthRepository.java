41f9a1d98eb33745c0cc1ae4ff4f30b7d7266e32
Paulo Prado <paulopraddo011@gmail.com>
2024-03-01 14:05:40 -0300

    Deletando o security, estava pulando etapas

--- src/main/java/com/livraria/Livraria/repositories/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.livraria.Livraria.repositories;
-
-import com.livraria.Livraria.entity.LoginUserDetailsModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthRepository extends JpaRepository<LoginUserDetailsModel, String> {
-}
