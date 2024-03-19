15537d36ee5806581fa6b71b508b5dc1dcb9e941
Paulo Prado <paulopraddo011@gmail.com>
2024-03-19 15:00:36 -0300

    deletando codigo quebrado

--- src/main/java/com/livraria/Livraria/livros/app/user/restModels/UserRegister.java
+++
@@ -1,5 +0,0 @@
-package com.livraria.Livraria.livros.app.user.restModels;
-
-public record UserRegister(String username, String nome, String  senha, String email, String endereco, String role) {
-
-}
--- src/main/java/com/livraria/Livraria/livros/domain/user/useCases/RegistrarNovoUsuarioUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.livraria.Livraria.livros.domain.user.useCases;
-
-import com.livraria.Livraria.livros.domain.user.models.LoginUserDetailsModel;
-
-public interface RegistrarNovoUsuarioUseCase {
-
-    LoginUserDetailsModel execute(LoginUserDetailsModel model);
-
-}
